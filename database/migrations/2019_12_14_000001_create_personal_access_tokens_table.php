a082b6c4aac88985bea81f697aee34ae65eddeda
Ruzman <roesman.labfik@umi.ac.id>
2023-02-05 03:28:50 +0800

    v0.0.0.7

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('tokenable_type', 191);
             $table->unsignedBigInteger('tokenable_id');
             $table->string('name');
