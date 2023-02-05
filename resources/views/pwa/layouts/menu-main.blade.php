b740767df64a8227df02b18bf0a68ef6565cb20d
Ruzman <roesman.labfik@umi.ac.id>
2023-02-06 06:21:32 +0800

    v0.0.0.13 : Payment Notif Fix

--- resources/views/pwa/layouts/menu-main.blade.php
+++ resources/views/pwa/layouts/menu-main.blade.php
@@ -5,41 +5,36 @@
     </div>
     <div class="card-bottom">
         <h1 class="color-white ps-3 mb-n1 font-28">FASTIX</h1>
-        <p class="mb-2 ps-3 font-12 color-white opacity-50">Welcome to the Future</p>
+        <p class="mb-2 ps-3 font-12 color-white opacity-50">Solusi Tepat, Tiket Cepat</p>
     </div>
     <div class="card-overlay bg-gradient"></div>
 </div>
 <div class="mt-4"></div>
-<h6 class="menu-divider">Library</h6>
+<h6 class="menu-divider">Menu</h6>
 <div class="list-group list-custom-small list-menu">
-    <a id="nav-welcome" href="index.html">
-        <i class="fa fa-heart gradient-red color-white"></i>
-        <span>Welcome</span>
+    <a id="nav-scan" href="{{route('cust.scan')}}" class="{{$scan ?? ''}}">
+        <i class="fa fa-qrcode gradient-red color-white" ></i>
+        <span>Scan</span>
         <i class="fa fa-angle-right"></i>
     </a>
-    <a id="nav-homepages" href="index-homepages.html">
+    <a id="nav-Riwayat" href="/" class="{{$riwayat ?? ''}}">
         <i class="fa fa-home gradient-green color-white"></i>
-        <span>Homepages</span>
+        <span>Riwayat</span>
         <i class="fa fa-angle-right"></i>
     </a>
-    <a id="nav-components" href="index-components.html">
-        <i class="fa fa-cog gradient-blue color-white"></i>
-        <span>Components</span>
+    <a id="nav-Transaksi" href="{{route('cust.transaction')}}" class="{{$transaksi ?? ''}}">
+        <i class="fa fa-money-check-alt gradient-blue color-white"></i>
+        <span>Transaksi</span>
         <i class="fa fa-angle-right"></i>
     </a>
-    <a id="nav-pages" href="index-pages.html" class="active-nav">
-        <i class="fa fa-file gradient-brown color-white"></i>
-        <span>Pages</span>
+    <a id="nav-Tiket" href="{{route('cust.ticket')}}" class="{{$ticket ?? ''}}">
+        <i class="fa fa-ticket gradient-brown color-white"></i>
+        <span>Tiket</span>
         <i class="fa fa-angle-right"></i>
     </a>
-    <a id="nav-media" href="index-projects.html">
-        <i class="fa fa-camera gradient-magenta color-white"></i>
-        <span>Media</span>
-        <i class="fa fa-angle-right"></i>
-    </a>
-    <a id="nav-contact" href="page-contact.html">
-        <i class="fa fa-envelope gradient-teal color-white"></i>
-        <span>Contact</span>
+    <a id="nav-Profile" href="{{route('cust.profile')}}" class="{{$profile ?? ''}}">
+        <i class="fa fa-person gradient-magenta color-white"></i>
+        <span>Profile</span>
         <i class="fa fa-angle-right"></i>
     </a>
 </div>
@@ -58,24 +53,11 @@
             <label class="custom-control-label" for="toggle-dark-menu"></label>
         </div>
     </a>
-</div>
-<h6 class="menu-divider mt-4">Contacts</h6>
-<div class="list-group list-custom-small list-menu">
-    <a href="#">
-        <img src="images/pictures/1s.jpg">
-        <span>John Droid</span>
-        <i class="fa fa-angle-right"></i>
-    </a>
-    <a href="#">
-        <img src="images/pictures/5s.jpg">
-        <span>Gina Seed</span>
-        <i class="fa fa-angle-right"></i>
-    </a>
-    <a href="#">
-        <img src="images/pictures/6s.jpg">
-        <span>Jane Louder</span>
+    <a id="nav-logout" href="{{route('logout')}}">
+        <i class="fa fa-envelope gradient-teal color-white"></i>
+        <span>Logout</span>
         <i class="fa fa-angle-right"></i>
     </a>
 </div>
-<h6 class="menu-divider font-10 mt-4">Made with <i class="fa fa-heart color-red-dark pl-1 pr-1"></i> by Enabled in
-    <span class="copyright-year">2021</span></h6>
+<h6 class="menu-divider font-10 mt-4">Made with <i class="fa fa-heart color-red-dark pl-1 pr-1"></i> by Fasitx
+    <span class="copyright-year">2023</span></h6>
